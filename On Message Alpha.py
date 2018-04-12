51ceb82c27e88b74252fd7eddb56da2c8466774d
mes10 <38220996+mes10@users.noreply.github.com>
2018-04-12 15:43:11 -0400

    Update On Message Alpha.py

--- On Message Alpha.py
+++ On Message Alpha.py
@@ -238,7 +238,9 @@ async def on_message(message):
 			embed.add_field(name="__Suggestion__", value= "```%s```" % (" ".join(args[1:])), inline=False)
 			embed.add_field(name="By", value="{0.author.mention}".format(message), inline=False)
 			embed.set_thumbnail(url='https://openclipart.org/image/2400px/svg_to_png/10515/yves-guillou-idea.png')
-			await client.send_message(channel, embed=embed)	
+			a=await client.send_message(channel, embed=embed)
+			await client.add_reaction(a, "✅")
+			await client.add_reaction(a, "⛔")	
 		
 
 		
