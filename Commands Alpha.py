5128707c876e0136318ff69c9b3aea010528d266
Trocirr <38221031+Trocirr@users.noreply.github.com>
2018-06-11 20:31:39 +0200

    Update Commands Alpha.py

--- Commands Alpha.py
+++ Commands Alpha.py
@@ -17,7 +17,10 @@ client.remove_command('help')
 
 @client.event
 async def on_ready():
-    print("Bot is ready!")
+    print('Logged in as')
+    print(client.user.name)
+    print(client.user.id)
+    print('------')
     
 	
 @client.event
@@ -40,63 +43,82 @@ async def time(ctx):
 	embed.add_field(name=":stopwatch: **Current Local Time and Date in the United Kingdom**", value=utc)
 	await client.say(embed=embed)	
 	
+
 	
 @client.command(pass_context=True)
 async def idk2(ctx):
-	embed = discord.Embed(title="Welcome to Infernal!", description="Infernal is launched in May of 2018 and was founded by Trocir. Infernal is led by Trocir. The clan does not use any extensions or mods and the players are popular for their legit and fair playstyle.", color=0xFF8C00)
+	embed = discord.Embed(title="Welcome to **Bonk.io Football League**!", description="BFL is launched in January of 2018. This is the official discord server of Bonk.io Football League. The league does not use any extensions or mods. You can talk about anything here as long as it isn't disrespectful.", color=0xFF8C00)
 	await client.say(embed=embed)	
 	
 
 @client.command(pass_context=True)
 async def exdee(ctx):
     embed = discord.Embed(description="", color=0xC9C9C9)
-    embed.add_field(name="Rules", value="1 - Post in the appropriate channels. \n2 - Excessive name calling, harassment, or threats will not be tolerated and will result in a punishment. \n3 - Staff and Moderators decisions are final. Please respect them and the processes they use. \n4 - No discussion about highly illegal activity. \n5 - Do not post any NSFW, malware, or any other content that may be deemed offensive. \n6 - Be respectful of each other and keep discussions constructive. \n7 - Do not spam. \n8 - No (random) links in the general chat #general unless a moderator or higher lets you!")
+    embed.add_field(name="Rules", value="You can talk about anything here as long as it isn't disrespectful. If you have a problem with anyone or anything in this server, please DM the Staff about it. \n \n 1. Rudeness, harassment, and immaturity will not be tolerated. \n 2. Porn and otherwise NSFW/NSFL content is prohibited. \n 3. No discussion about highly illegal activity. \n 4. Copyrighted and other unlawful material is prohibited, such as links to illegal content. \n 5. Advertising and promoting is prohibited. Don't even bother posting invite links because the bots will delete them anyway. \n 6. <#443154096787161092> is our main channel. Keep it clean. \n 7. No (random) links in the general chat <#443154096787161092> unless a moderator or higher lets you!")
     await client.say(embed=embed)
 	
 	
 @client.command(pass_context=True)
 async def idk(ctx):
     embed = discord.Embed(description="", color=0xC9C9C9)
-    embed.add_field(name="Roles", value="<@&442436870656360469> - The Leader of the team \n <@&442436324402659328> - Official Members \n <@&442648846338752513> - Official Bot \n <@&442442489404850176> - Important People \n <@&442443800447549442> - Special People \n <@&442440578710372352> - Special Guests \n <@&442440784709550083> - The girls \n <@&437282617885458432> - Default role \n <@&436460911192965121> - Server bots")
+    embed.add_field(name="Roles", value="<@&443070361450446848> - Staff Team of the league \n <@&443436497350164490> - Referees, tag them if you need referee! \n <@&443074283837718546> - Official BFL Bot \n <@&443070319045902357> - BFL League Players \n <@&443070027743232001> - Default role \n <@&443070606657716234> - Role for our bots")
     await client.say(embed=embed)	
 	
 	
 @client.command(pass_context=True, description='Shows the server info.')
 async def server(ctx):
-	embed = discord.Embed(description="Here's what I could find:", color=0x00ff00)
-	embed.add_field(name="Name", value=ctx.message.server.name)
-	embed.add_field(name="Owner", value=ctx.message.server.owner)
-	embed.add_field(name="Region", value=ctx.message.server.region)
-	embed.add_field(name="Roles", value=len(ctx.message.server.roles))
-	embed.add_field(name="Members", value=len(ctx.message.server.members))
-	embed.add_field(name="Channels", value=len(ctx.message.server.channels))
-	embed.set_thumbnail(url=ctx.message.server.icon_url)
-	await client.say(embed=embed)
+    embed = discord.Embed(description="Here's what I could find:", color=0x00ff00)
+    embed.add_field(name="Name", value=ctx.message.server.name)
+    embed.add_field(name="Owner", value=ctx.message.server.owner)
+    embed.add_field(name="Region", value=ctx.message.server.region)
+    embed.add_field(name="Roles", value=len(ctx.message.server.roles))
+    embed.add_field(name="Members", value=len(ctx.message.server.members))
+    embed.add_field(name="Channels", value=len(ctx.message.server.channels))
+    embed.set_thumbnail(url=ctx.message.server.icon_url)
+    utc_dt = datetime.now(timezone.utc)
+    p = utc_dt.strftime('     Time - %H:%M:%S | Date - %d/%m/%Y')
+    utc = str(p)    
+    a=ctx.message.author
+    txt= str(a) + " | " + str(utc)
+    embed.set_footer(text=txt, icon_url=ctx.message.author.avatar_url)
+    await client.say(embed=embed)
+	
 	
 	
 @client.command(pass_context=True, description='Shows the info of the mentioned user.')
 async def info(ctx, user: discord.Member):
-	embed = discord.Embed(title="{}'s info".format(user.name), description="Here's what I could find:", color=0x00ff00)
-	embed.add_field(name="Name", value=user.name, inline=True)
-	embed.add_field(name="ID", value=user.id, inline=True)
-	embed.add_field(name="Status", value=user.status, inline=True)
-	embed.add_field(name="Highest role", value=user.top_role)
-	embed.add_field(name="Joined",value=user.joined_at)
-	embed.set_thumbnail(url = user.avatar_url)
-	await client.say(embed=embed)
+    embed = discord.Embed(title="{}'s info".format(user.name), description="Here's what I could find:", color=0x00ff00)
+    embed.add_field(name="Name", value=user.name, inline=True)
+    embed.add_field(name="ID", value=user.id, inline=True)
+    embed.add_field(name="Status", value=user.status, inline=True)
+    embed.add_field(name="Highest role", value=user.top_role)
+    embed.add_field(name="Joined",value=user.joined_at)
+    embed.set_thumbnail(url = user.avatar_url)
+    utc_dt = datetime.now(timezone.utc)
+    p = utc_dt.strftime('     Time - %H:%M:%S | Date - %d/%m/%Y')
+    utc = str(p)    
+    a=ctx.message.author
+    txt= str(a) + " | " + str(utc)
+    embed.set_footer(text=txt, icon_url=ctx.message.author.avatar_url)    
+    await client.say(embed=embed)
 	
 	
 @client.command(pass_context=True)
 async def about(ctx):
-	embed = discord.Embed(title="__About Feed Me__", description="© 2018 Feed Me Bot All Rights Reserved", color=0x00ff00)
-	embed.add_field(name="Original Creators", value="<@399567243744116738> & <@293447483818901504>")
-	embed.add_field(name="Co-Creator", value="<@331876823443177473>")
-	embed.add_field(name="Server count", value=str(len(client.servers)))
-	embed.add_field(name="User count", value=str(len(list(client.get_all_members()))))
-	embed.add_field(name="Channel count",value=str(len(list(client.get_all_channels()))))
-	embed.add_field(name="API", value="Python")
-	embed.set_thumbnail(url='https://media.discordapp.net/attachments/438003912184692738/438306520317427712/Test.png?width=334&height=341')
-	await client.say(embed=embed)
+    embed = discord.Embed(title="__About BFL__", description="© 2018 BFL Bot All Rights Reserved", color=0x00ff00)
+    embed.add_field(name="Original Creators", value="<@399567243744116738> & <@293447483818901504>")
+    embed.add_field(name="Server count", value=str(len(client.servers)))
+    embed.add_field(name="User count", value=str(len(list(client.get_all_members()))))
+    embed.add_field(name="Channel count",value=str(len(list(client.get_all_channels()))))
+    embed.add_field(name="API", value="Python")
+    embed.set_thumbnail(url='https://media.discordapp.net/attachments/438003912184692738/438306520317427712/Test.png?width=334&height=341')
+    utc_dt = datetime.now(timezone.utc)
+    p = utc_dt.strftime('     Time - %H:%M:%S | Date - %d/%m/%Y')
+    utc = str(p)    
+    a=ctx.message.author
+    txt= str(a) + " | " + str(utc)
+    embed.set_footer(text=txt, icon_url=ctx.message.author.avatar_url) 
+    await client.say(embed=embed)
 	
 	
 @client.command(pass_context=True)
@@ -106,7 +128,7 @@ async def trocir(ctx):
     embed.add_field(name="Goals", value="0")
     embed.add_field(name="Assists", value="0")
     embed.add_field(name="Own Goals", value="0")
-    embed.set_thumbnail(url="https://media.discordapp.net/attachments/443418563160571944/443423107215261697/Feed_Me_Bonk_League.png?width=462&height=420")
+    embed.set_thumbnail(url="https://media.discordapp.net/attachments/443418563160571944/446395067373191169/Picture1.png?width=868&height=646")
     await client.say(embed=embed)
 	
 
@@ -114,21 +136,21 @@ async def trocir(ctx):
 async def microhype(ctx):
     embed = discord.Embed(title="__MicroHype's profile__ :flag_gb:", description="", color=0x2874A6)
     embed.add_field(name="Joined", value="7th May 2018")
-    embed.add_field(name="Goals", value="12")
+    embed.add_field(name="Goals", value="0")
     embed.add_field(name="Assists", value="0")
-    embed.add_field(name="Own Goals", value="2")
-    embed.set_thumbnail(url="https://media.discordapp.net/attachments/443418563160571944/443423107215261697/Feed_Me_Bonk_League.png?width=462&height=420")
+    embed.add_field(name="Own Goals", value="0")
+    embed.set_thumbnail(url="https://media.discordapp.net/attachments/443418563160571944/446395067373191169/Picture1.png?width=868&height=646")
     await client.say(embed=embed)	
 	
 	
 @client.command(pass_context=True)
-async def sm4ck(ctx):
-    embed = discord.Embed(title="__Sm4ck's profile__ :flag_ps:", description="", color=0x2874A6)
+async def frozenkiller(ctx):
+    embed = discord.Embed(title="__frozenkiller's profile__ :flag_nl:", description="", color=0x2874A6)
     embed.add_field(name="Joined", value="7th May 2018")
     embed.add_field(name="Goals", value="0")
     embed.add_field(name="Assists", value="0")
     embed.add_field(name="Own Goals", value="0")
-    embed.set_thumbnail(url="https://media.discordapp.net/attachments/443418563160571944/443423107215261697/Feed_Me_Bonk_League.png?width=462&height=420")
+    embed.set_thumbnail(url="https://media.discordapp.net/attachments/443418563160571944/446395067373191169/Picture1.png?width=868&height=646")
     await client.say(embed=embed)
 	
 
@@ -139,7 +161,7 @@ async def avenger434(ctx):
     embed.add_field(name="Goals", value="0")
     embed.add_field(name="Assists", value="0")
     embed.add_field(name="Own Goals", value="0")
-    embed.set_thumbnail(url="https://media.discordapp.net/attachments/443418563160571944/443423107215261697/Feed_Me_Bonk_League.png?width=462&height=420")
+    embed.set_thumbnail(url="https://media.discordapp.net/attachments/443418563160571944/446395067373191169/Picture1.png?width=868&height=646")
     await client.say(embed=embed)
 	
 	
@@ -147,10 +169,10 @@ async def avenger434(ctx):
 async def mhmmyeeesss(ctx):
     embed = discord.Embed(title="__MhmmYeeesss's profile__ :flag_us:", description="", color=0x2874A6)
     embed.add_field(name="Joined", value="7th May 2018")
-    embed.add_field(name="Goals", value="21")
+    embed.add_field(name="Goals", value="0")
     embed.add_field(name="Assists", value="0")
     embed.add_field(name="Own Goals", value="0")
-    embed.set_thumbnail(url="https://media.discordapp.net/attachments/443418563160571944/443423107215261697/Feed_Me_Bonk_League.png?width=462&height=420")
+    embed.set_thumbnail(url="https://media.discordapp.net/attachments/443418563160571944/446395067373191169/Picture1.png?width=868&height=646")
     await client.say(embed=embed)	
 	
 	
@@ -158,10 +180,10 @@ async def mhmmyeeesss(ctx):
 async def acquah(ctx):
     embed = discord.Embed(title="__Acquah's profile__ :flag_it:", description="", color=0x2874A6)
     embed.add_field(name="Joined", value="7th May 2018")
-    embed.add_field(name="Goals", value="14")
+    embed.add_field(name="Goals", value="0")
     embed.add_field(name="Assists", value="0")
-    embed.add_field(name="Own Goals", value="3")
-    embed.set_thumbnail(url="https://media.discordapp.net/attachments/443418563160571944/443423107215261697/Feed_Me_Bonk_League.png?width=462&height=420")
+    embed.add_field(name="Own Goals", value="0")
+    embed.set_thumbnail(url="https://media.discordapp.net/attachments/443418563160571944/446395067373191169/Picture1.png?width=868&height=646")
     await client.say(embed=embed)	
 	
 	
@@ -172,7 +194,7 @@ async def reddragon6(ctx):
     embed.add_field(name="Goals", value="0")
     embed.add_field(name="Assists", value="0")
     embed.add_field(name="Own Goals", value="0")
-    embed.set_thumbnail(url="https://media.discordapp.net/attachments/443418563160571944/443423107215261697/Feed_Me_Bonk_League.png?width=462&height=420")
+    embed.set_thumbnail(url="https://media.discordapp.net/attachments/443418563160571944/446395067373191169/Picture1.png?width=868&height=646")
     await client.say(embed=embed)
 	
 	
@@ -183,7 +205,7 @@ async def alex2810(ctx):
     embed.add_field(name="Goals", value="0")
     embed.add_field(name="Assists", value="0")
     embed.add_field(name="Own Goals", value="0")
-    embed.set_thumbnail(url="https://media.discordapp.net/attachments/443418563160571944/443423107215261697/Feed_Me_Bonk_League.png?width=462&height=420")
+    embed.set_thumbnail(url="https://media.discordapp.net/attachments/443418563160571944/446395067373191169/Picture1.png?width=868&height=646")
     await client.say(embed=embed)	
 	
 	
@@ -194,7 +216,7 @@ async def nahojaa(ctx):
     embed.add_field(name="Goals", value="0")
     embed.add_field(name="Assists", value="0")
     embed.add_field(name="Own Goals", value="0")
-    embed.set_thumbnail(url="https://media.discordapp.net/attachments/443418563160571944/443423107215261697/Feed_Me_Bonk_League.png?width=462&height=420")
+    embed.set_thumbnail(url="https://media.discordapp.net/attachments/443418563160571944/446395067373191169/Picture1.png?width=868&height=646")
     await client.say(embed=embed)	
 	
 	
@@ -205,7 +227,7 @@ async def numbskullian(ctx):
     embed.add_field(name="Goals", value="0")
     embed.add_field(name="Assists", value="0")
     embed.add_field(name="Own Goals", value="0")
-    embed.set_thumbnail(url="https://media.discordapp.net/attachments/443418563160571944/443423107215261697/Feed_Me_Bonk_League.png?width=462&height=420")
+    embed.set_thumbnail(url="https://media.discordapp.net/attachments/443418563160571944/446395067373191169/Picture1.png?width=868&height=646")
     await client.say(embed=embed)	
 	
 
@@ -216,18 +238,39 @@ async def nasean79(ctx):
     embed.add_field(name="Goals", value="0")
     embed.add_field(name="Assists", value="0")
     embed.add_field(name="Own Goals", value="0")
-    embed.set_thumbnail(url="https://media.discordapp.net/attachments/443418563160571944/443423107215261697/Feed_Me_Bonk_League.png?width=462&height=420")
+    embed.set_thumbnail(url="https://media.discordapp.net/attachments/443418563160571944/446395067373191169/Picture1.png?width=868&height=646")
     await client.say(embed=embed)		
+		
 	
+@client.command(pass_context=True)
+async def commanderjoash(ctx):
+    embed = discord.Embed(title="__CommanderJoash's profile__ :flag_us:", description="", color=0x2874A6)
+    embed.add_field(name="Joined", value="8th May 2018")
+    embed.add_field(name="Goals", value="0")
+    embed.add_field(name="Assists", value="0")
+    embed.add_field(name="Own Goals", value="0")
+    embed.set_thumbnail(url="https://media.discordapp.net/attachments/443418563160571944/446395067373191169/Picture1.png?width=868&height=646")
+    await client.say(embed=embed)	
+	
+	
+@client.command(pass_context=True)
+async def aggthescorer(ctx):
+    embed = discord.Embed(title="__AggTheScorer__ :flag_gr:", description="", color=0x2874A6)
+    embed.add_field(name="Joined", value="15th May 2018")
+    embed.add_field(name="Goals", value="0")
+    embed.add_field(name="Assists", value="0")
+    embed.add_field(name="Own Goals", value="0")
+    embed.set_thumbnail(url="https://media.discordapp.net/attachments/443418563160571944/446395067373191169/Picture1.png?width=868&height=646")
+    await client.say(embed=embed)	
 	
 	
 @client.command(pass_context=True)
 async def records(ctx):
-    embed = discord.Embed(title="__Feed Me Records__", description="Our Football Records", color=0x2874A6)
+    embed = discord.Embed(title="__BFL Records__", description="Our Football Records", color=0x2874A6)
     embed.add_field(name="Biggest Win", value="Team 0-0 Team")
-    embed.add_field(name="Most Goals in a match", value="MicroHype - 8", inline = False)
+    embed.add_field(name="Most Goals in a match", value="Player - 0", inline = False)
     embed.add_field(name="Most Assists in a match", value="Player - 0", inline = False)
-    embed.add_field(name="Most Goals of all time", value="Mhmm Yeeesss - 21", inline = False)
+    embed.add_field(name="Most Goals of all time", value="Player - 0", inline = False)
     embed.add_field(name="Most Assists of all time", value="Player - 0", inline = False)
     embed.set_thumbnail(url="https://media.discordapp.net/attachments/443418563160571944/443423107215261697/Feed_Me_Bonk_League.png?width=462&height=420")
     await client.say(embed=embed)
@@ -236,9 +279,15 @@ async def records(ctx):
 	
 @client.command(pass_context=True)
 async def avatar(ctx, user: discord.Member):
-	embed = discord.Embed(title="{}'s avatar".format(user.name), color=0x00BFFF)
-	embed.set_image(url = user.avatar_url)
-	await client.say(embed=embed)	
+    embed = discord.Embed(title="{}'s avatar".format(user.name), color=0x00BFFF)
+    embed.set_image(url = user.avatar_url)
+    utc_dt = datetime.now(timezone.utc)
+    p = utc_dt.strftime('     Time - %H:%M:%S | Date - %d/%m/%Y')
+    utc = str(p)    
+    a=ctx.message.author
+    txt= str(a) + " | " + str(utc)
+    embed.set_footer(text=txt, icon_url=ctx.message.author.avatar_url) 
+    await client.say(embed=embed)	
 	
 	
 	
@@ -288,12 +337,12 @@ async def choose(ctx,message):
 @client.command(pass_context=True)
 @commands.cooldown(1, 5, commands.BucketType.user)
 async def ping(ctx):
-	channel = ctx.message.channel
-	t1 = time.perf_counter()
-	await client.send_typing(channel)
-	t2 = time.perf_counter()
-	embed=discord.Embed(title=None, description=':ping_pong: Pong! `{}ms`'.format(round((t2-t1)*1000)), color=0x2874A6)
-	await client.say(embed=embed)
+    channel = ctx.message.channel
+    t1 = time.perf_counter()
+    await client.send_typing(channel)
+    t2 = time.perf_counter()
+    embed=discord.Embed(title=None, description=':ping_pong: Pong! `{}ms`'.format(round((t2-t1)*1000)), color=0x2874A6)
+    await client.say(embed=embed)
 
 @client.command(pass_context=True)
 @commands.cooldown(1, 10, commands.BucketType.user)
@@ -314,11 +363,10 @@ async def urban(*, word: str):
 		example = defi[0]['example']
 		embed = discord.Embed(title=word, description=definition, color=0x0062f4)
 		embed.add_field(name="Example", value=example, inline=False)
-		embed.set_footer(text="Urban Dictionary")
 		await client.say(embed=embed)
 	except:
 		await client.say(":exclamation: Error. No definition found.")
 			
 
 
-client.run("NDQzMDczNzMyMTg2MDc5MjM1.DdIETg._dnErZuMYPs9zWGVhMVOPOF7i6Q")
+client.run("Mzk2NDU4MTA4OTg3MjQ0NTYz.Dd4QJg.NQa0nFp4EXk-aq9J5bMS2KDdxbU")
